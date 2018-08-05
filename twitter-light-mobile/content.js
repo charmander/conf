15cd27067fbea2db0e1f8ac6494545f51908faed
Charmander <~@charmander.me>
2018-08-04 23:43:06 -0700

    Fix “Display sensitive media” button on light mobile.twitter.com
    The usual timeline button requires cookies, and the button on /media/grid just doesn’t work at all (it doesn’t accept the attempted POST). Both allow `show_media=1` to be specified as a querystring parameter, though.

---
+++ twitter-light-mobile/content.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const hidden = (name, value) =>
+	Object.assign(document.createElement('input'), {
+		type: 'hidden',
+		name,
+		value,
+	});
+
+const form = document.createElement('form');
+form.id = 'show-media-fix-form';
+form.hidden = true;
+
+const params = new URLSearchParams(location.search);
+
+for (const [key, value] of params) {
+	form.appendChild(hidden(key, value));
+}
+
+form.appendChild(hidden('show_media', '1'));
+
+document.body.appendChild(form);
+
+for (const button of document.querySelectorAll('input[name="commit"][value="Display media"]')) {
+	button.setAttribute('form', form.id);
+	button.name = '';
+}
