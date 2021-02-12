847f1d98aa3aee08db8f0fe8e1cdfb83c578447f
Charmander <~@charmander.me>
2021-02-11 20:12:11 -0800

    google-plain-url: Disable scripts without relying on other extensions/configuration

--- browser-extensions/google-plain-url/background.js
+++ browser-extensions/google-plain-url/background.js
@@ -1,5 +1,22 @@
 'use strict';
 
+browser.webRequest.onHeadersReceived.addListener(
+	({responseHeaders}) => {
+		responseHeaders.push({
+			name: 'Content-Security-Policy',
+			// TODO: check whether the specific directives are necessary once implemented
+			value: "script-src 'none'; script-src-elem 'none'; script-src-attr 'none'",
+		});
+
+		return {responseHeaders};
+	},
+	{
+		types: ['main_frame'],
+		urls: ['https://www.google.com/*'],
+	},
+	['blocking', 'responseHeaders'],
+);
+
 browser.webRequest.onBeforeRequest.addListener(
 	request => ({cancel: true}),
 	{urls: ['https://www.google.com/url?*']},
